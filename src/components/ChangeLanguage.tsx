231fce9a4703a4d87638a5f0ae7a7da45b34f457
Luis Herrera <luis1818.lh@gmail.com>
2024-06-04 21:14:36 -0500

    configuration tailwind css and create componet ChangeLanguage

---
+++ src/components/ChangeLanguage.tsx
@@ -0,0 +1,12 @@
+
+
+export const ChangeLanguage = () => {
+  return (
+    <section>
+        <select name="select">
+            <option value="value1">1</option>
+            <option value="value2">2</option>
+        </select>
+    </section>
+  )
+}
